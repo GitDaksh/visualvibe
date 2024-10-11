d3ff898708049e902518d824c51f801a668fc48e
Daksh <daksh.java.util@gmail.com>
2024-10-11 19:35:27 +0530

    Meeting Calls Setup
    create meeting calls page and functionality of buttons, worked on layouts.

--- app/(root)/(home)/layout.tsx
+++ app/(root)/(home)/layout.tsx
@@ -1,6 +1,15 @@
 import Navbar from '@/components/Navbar'
 import Sidebar from '@/components/Sidebar'
+import { Metadata } from 'next';
 import React, { ReactNode } from 'react'
+
+export const metadata: Metadata = {
+  title: "VisualVibe",
+  description: "Video Conferencing Web App",
+  icons: {
+    icon: '/icons/logo.svg'
+  }
+};
 
 const HomeLayout = ({ children }: {children: ReactNode}) => {
   return (
--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,14 @@
 import StreamVideoProvider from '@/providers/StreamVideoProvider'
+import { Metadata } from 'next';
 import React, { ReactNode } from 'react'
+
+export const metadata: Metadata = {
+  title: "VisualVibe",
+  description: "Video Conferencing Web App",
+  icons: {
+    icon: '/icons/logo.svg'
+  }
+};
 
 const RootLayout = ({ children }: {children: ReactNode}) => {
   return (

2cab538d003365343041d77d7f5a826156d5a9b5
Daksh <daksh.java.util@gmail.com>
2024-10-14 18:26:02 +0530

    personal rooms
    created personal meeting rooms, recording tab and CallList

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -11,7 +11,7 @@ import {
     DropdownMenuTrigger,
   } from "@/components/ui/dropdown-menu"
 import { LayoutList, Users } from 'lucide-react';
-import { useSearchParams } from 'next/navigation';
+import { useRouter, useSearchParams } from 'next/navigation';
 import EndCallButton from './EndCallButton';
 import Loader from './Loader';
   
@@ -24,6 +24,7 @@ const MeetingRoom = () => {
 
     const [layout, setLayout] = useState('speaker-left');
     const [showParticipants, setShowParticipants] = useState(false);
+    const router = useRouter();
 
     const { useCallCallingState } = useCallStateHooks();
     const callingState = useCallCallingState();
@@ -53,7 +54,7 @@ const MeetingRoom = () => {
         </div>
 
         <div className='fixed bottom-0 flex w-full items-center justify-center gap-5 flex-wrap'>
-            <CallControls />
+            <CallControls onLeave = {() => router.push('/')}/>
 
             <DropdownMenu>
                 <div className='flex items-center'>
--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -9,6 +9,7 @@ import { Call, useStreamVideoClient } from "@stream-io/video-react-sdk";
 import { useToast } from "@/hooks/use-toast"
 import { Textarea } from "./ui/textarea";
 import ReactDatePicker from 'react-datepicker';
+import { Input } from "./ui/input";
 
 const MeetingTypeList = () => {
   const router = useRouter();
@@ -42,7 +43,7 @@ const MeetingTypeList = () => {
 
         if(!call) throw new Error('Failed to create call')
 
-        const startsAt = values.dateTime.toISOString() || new Date(Date.now()).toISOString();
+        const startsAt = values.dateTime.toISOString() || new Date(Date.now()).toISOString(); 
         const description = values.description || 'Instant meeting';
 
         await call.getOrCreate({
@@ -94,7 +95,7 @@ const MeetingTypeList = () => {
             img = '/icons/recordings.svg'
             title = 'View Recordings'
             description = 'Check out your recordings'
-            handleClick = {() => setMeetingState('isJoiningMeeting')}
+            handleClick = {() => router.push('/recordings')}
             className = 'bg-purple-1'
             
         />
@@ -160,6 +161,23 @@ const MeetingTypeList = () => {
             buttonText = 'Start Meeting'
             handleClick = {createMeeting}
         />
+
+
+        <MeetingModal 
+            isOpen = {meetingState === 'isJoiningMeeting'}
+            onClose = {() => setMeetingState(undefined)}
+            title = "Type the link here"
+            className = 'text-center'
+            buttonText = 'Join Meeting'
+            handleClick = {() => router.push(values.link)}
+        >
+            <Input 
+            placeholder="Meeting link"
+            className="border-none bg-dark-3 focus-visible:ring-0 focus-visible:ring-offset-0"    
+            onChange={(e) => setvalues({...values, link: e.target.value })}
+        />
+        </MeetingModal>
+        
     </section>
   )
 }
